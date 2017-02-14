cf4b3307c56e18b0c56f7c431798b1e76dd7e8e8
RosaJer <verylittlejez@gmail.com>
2017-02-14 01:50:29 +0000

    poo

--- poo.js
+++ poo.js
@@ -90,11 +90,23 @@ var scrolls = [
 	},
 	"prefix":false
 },
+{
+	"name":"Ornate",
+	"locations":["weapon"],
+	"types":["longhammer"],
+	"stats":{
+		"balance":7,
+		"speed":5,
+		"crit":3
+	},
+	"prefix":true
+},
 ]
 
 var weapons = [
 {
 	"name":"Poo Longsword",
+		"type":"longsword",
 	"stats":{
 		"speed":9,
 		"crit":39,
@@ -103,6 +115,7 @@ var weapons = [
 },
 {
 	"name":"Poo Hammer",
+	"type":"longhammer",
 	"stats":{
 		"speed":9,
 		"crit":42,
@@ -119,6 +132,28 @@ function createInputBox()
 	return ret
 }
 
+function passFilter(loc, type, scr, prefix)
+{
+	// loc not in scroll locs
+	if(-1 == $.inArray(loc,scr["locations"]))
+	{
+		return false;
+	}
+	
+	// scroll has a types filter and item fails it
+	if (type && "types" in scr && $.inArray(type, scr["types"]) == -1)
+	{
+		return false
+	}
+	
+	if (scr["prefix"] != prefix)
+	{
+		return false
+	}
+	
+	return true;
+}
+
 function createSquare(id, loc)
 {	
 	var target = $("#"+id)
@@ -130,15 +165,118 @@ function createSquare(id, loc)
 	box.css("background-size", "cover")
 	
 	var prefix = createInputBox()
-	prefix.autocomplete({source:["Immoral", "Leopard"]})
 	var suffix = createInputBox()
-	suffix.autocomplete({source:["Valor", "Budblust"]})
 	var item = createInputBox()
-	item.autocomplete({source:["Poo hammer","Poo longsword"]})
+	var display = $("<div />")
+	display.css("background-color",'white')
+	
+	var update = function ()
+	{
+		var p = box.data("prefix")
+		var s = box.data("suffix")
+		var w = box.data("weapon")
+		
+		var bal = 0
+		var crit = 0
+		var spd =0 
+		
+		$.each([p,s,w], function(k,x)
+		{
+			if(x && x.stats)
+			{
+				bal += x.stats.balance || 0
+				crit += x.stats.crit || 0 
+				spd += x.stats.spd|| 0
+			}
+		})
+		
+		display.html(["bal="+bal, "crit="+crit, "spd="+spd].join(","))
+	}
+	
+	var scrollSet = function(key, thing)
+	{
+		return function(){
+			var name = thing.val().toLowerCase()
+			$.each(scrolls, function(k,v)
+			{
+				if (v.name.toLowerCase() == name)
+				{
+					box.data(key, v)
+					return false 
+				}
+			})
+			update()
+		}
+	}
+	
+	var scrollSource = function (prefix){
+	return function(state, cb)
+		{
+			var term = $.ui.autocomplete.escapeRegex(state.term)
+			var re = new RegExp(term, "i")
+			var ret = []
+			var weapon = box.data("weapon") || {"type":null}
+			var type = weapon["type"]
+			$.each(scrolls, function(k,v)
+			{
+				if ( passFilter(loc, type, v, prefix) )
+				{
+					if (-1 != v["name"].search(re))
+					{
+						ret.push(v["name"])
+					}
+				}
+			})
+			cb(ret)
+		}
+	}
+	
+	prefix.autocomplete( {
+		source: scrollSource(true),
+		change: scrollSet("prefix", prefix),
+		"close": scrollSet("prefix", prefix),
+		delay: 250,
+		minLength:0,
+		position: {my:"left top", at :"right bottom"}
+	}
+	)
+	prefix.on("focus", function(){	prefix.autocomplete("search", "")})
+	
+	suffix.autocomplete({source: scrollSource(false),
+	position: {my:"left top", at :"right bottom"},
+	change: scrollSet("suffix", suffix),
+	"close": scrollSet("suffix", suffix),
+	minLength:0,
+	})
+	suffix.on("focus", function(){	suffix.autocomplete("search", "")})
+	
+	var updateWeapon = function()
+	{
+		var name = item.val().toLowerCase()
+		$.each(weapons, function(k,v)
+		{
+			if (v.name.toLowerCase() == name)
+			{
+				box.data("weapon", v)
+				return false 
+			}
+		})
+		update()
+	}
+	
+	item.autocomplete({source:["Poo hammer","Poo longsword"], 
+	"close":updateWeapon, 
+	"change":updateWeapon,
+	minLength:0,
+	position: {my:"left top", at :"right bottom"}
+	})
+	item.on("focus", function(){item.autocomplete("search", "")})
+	
 	
 	box.append(prefix)
 	box.append(suffix)
 	box.append(item)
+	box.append(display)
 	
 	target.append(box)
 }
